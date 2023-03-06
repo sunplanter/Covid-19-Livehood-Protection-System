3892a2b9809a680b138494130244d65a78655d13
Sunsplanter <sunsplanter@gmail.com>
2023-03-06 14:35:57 +0800

    1st to upload project

---
+++ vue/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+          TITLE
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
